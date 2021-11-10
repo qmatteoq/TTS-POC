306c617b2a4856bea02ecc12349c04176d1c420d
Robert Evans <robertev@microsoft.com>
2021-11-10 14:45:52 -0500

    Adding a net 6 example of 3d model... hope this works for us somehow...

---
+++ 3d/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using HelixToolkit.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _3dModelHelloWorld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //Path to the model file
+        private const string MODEL_PATH = "Model.stl";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ModelVisual3D device3D = new ModelVisual3D();
+            device3D.Content = Display3d(MODEL_PATH);
+            // Add to view port
+            viewPort3d.Children.Add(device3D);
+        }
+
+        /// <summary>
+        /// Display 3D Model
+        /// </summary>
+        /// <param name="model">Path to the Model file</param>
+        /// <returns>3D Model Content</returns>
+        private Model3D Display3d(string model)
+        {
+            Model3D device = null;
+            try
+            {
+                //Adding a gesture here
+                viewPort3d.RotateGesture = new MouseGesture(MouseAction.LeftClick);
+
+                //Import 3D model file
+                ModelImporter import = new ModelImporter();
+
+                //Load the 3D model file
+                device = import.Load(model);
+            }
+            catch (Exception e)
+            {
+                // Handle exception in case can not file 3D model
+                MessageBox.Show("Exception Error : " + e.StackTrace);
+            }
+            return device;
+        }
+    }
+}
